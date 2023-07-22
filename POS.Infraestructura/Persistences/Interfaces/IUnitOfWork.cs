bab7f57f9e39550653da530d840b11ae977a6d67
Edson MZ <emz19.com@gmail.com>
2023-07-21 21:01:46 -0600

    Added unitfowork pattern

---
+++ POS.Infraestructura/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        //se declara o matricula las interfaces a nivel de repositorio
+        ICategoryRepository Category{ get; }
+
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
---
+++ POS.Infraestructura/Persistences/Repositories/UnitOfWork.cs
@@ -0,0 +1,32 @@
+﻿using POS.Infrastructure.Persistences.Context;
+using POS.Infrastructure.Persistences.Interfaces;
+
+namespace POS.Infrastructure.Persistences.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly PosContext _context;
+        public ICategoryRepository Category { get; private set; }
+
+        public UnitOfWork(PosContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            Category = new CategoryRepository(context);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+           await _context.SaveChangesAsync();
+        }
+    }
+}
